0a14a7d97f967cd66abe88a91ece02a740d28298
bryanvargasv <bryan.vargasv@yahoo.com>
2021-02-17 20:13:52 -0300

    Creacion de proyecto springboot y entidad Alumno

---
+++ src/main/java/com/belerofonte/microservicios/app/usuarios/belerofontemicroserviciosusuario/BelerofonteMicroserviciosUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.belerofonte.microservicios.app.usuarios.belerofontemicroserviciosusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelerofonteMicroserviciosUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelerofonteMicroserviciosUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belerofonte/microservicios/app/usuarios/belerofontemicroserviciosusuario/BelerofonteMicroserviciosUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belerofonte.microservicios.app.usuarios.belerofontemicroserviciosusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelerofonteMicroserviciosUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
